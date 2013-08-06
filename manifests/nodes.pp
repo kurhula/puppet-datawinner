9567028aa0db31e7171fd2a3af7ba1d3cc9176ba
anishek.agarwal <anishek@gmail.com>
2013-08-06 17:56:19 +0530

    anishek/ashwin | correcting listen address param for server

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -18,7 +18,7 @@ node base_system {
 ######## Postgres installation ############
   class { "postgresql::server":
     config_hash => {
-      'listen_address' => '*',
+      'listen_addresses' => '*',
       'postgres_password' => 'postgres',
       'ip_mask_allow_all_users' => '0.0.0.0/0',
       'ip_mask_deny_postgres_user' => '0.0.0.0/32',
