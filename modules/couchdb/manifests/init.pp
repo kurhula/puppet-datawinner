9f7265f926c4956c083b7f77e7266ca06d5b9cea
ashwin <ashwin@ashwin.(none)>
2013-08-07 16:50:06 +0530

    Anishek/Ashwin | Adding scripts to deploy couchdb using new package.

--- modules/couchdb/manifests/init.pp
+++ modules/couchdb/manifests/init.pp
@@ -1,12 +1,36 @@
 class couchdb (
-  $package_location = 'https://github.com/mangroveorg/dwpkg/blob/master/apache-couchdb_1.3.0-1_amd64.deb',
-  $package_name = 'apache-couchdb_1.3.0-1_amd64.deb'
+  $package_location = 'https://github.com/mangroveorg/dwpkg/raw/master/couchdb/apache-couchdb_1.3.1-1_amd64.deb',
+  $package_name = 'apache-couchdb_1.3.1-1_amd64.deb'
 
-) {
+)
+ {
+  package {
+        ['wget','libicu-dev', 'libmozjs185-dev', 'libcurl4-gnutls-dev', 'libtool', 'erlang-base-hipe','erlang-eunit','erlang-nox','erlang-xmerl', 'erlang-inets']:	
+	ensure => 'installed',
+  }
+
   exec { 'download':
-    require => Package['curl'],
+    require => Package['wget'],
     cwd => '/opt/',
-    command => "wget -q ${package_loaction} -O ${package_name}",
-    timeout => 120
+    path => [ '/usr/local/bin', '/usr/bin', '/bin', ],   
+    command => "wget -q ${package_location} -O ${package_name}",
+    timeout => 120,
+  }
+  
+  file { '/opt/${package_name}' :
+    require => Exec['download'],
+    ensure => 'present',
+    mode => '0755',	
+  }
+  
+  exec { 'install-couchdb':
+   require => File['/opt/${package_name}'],
+   path => [ '/usr/local/bin', '/usr/sbin', '/sbin', '/usr/bin', '/bin', ],
+   cwd => '/opt/',
+   command => "dpkg -i ${package_name}",
+   timeout => 120,		
   }
 }
+
+
+
