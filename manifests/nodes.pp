0131b47a44d71ca7a8efb8fd2a7626e6c488be38
admin <test@test-VirtualBox.(none)>
2013-08-09 12:05:26 +0530

    Anishek/Ashwin | Correcting permission issues for various files in couchdb installation

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,95 +1,94 @@
 node base_system {
-  package { 'git-core':
-    ensure => installed,
-  }
-  package {'curl':
-  ensure => installed
-  }
+  package { 'git-core': ensure => installed, }
 
-  group{"datawinners": ensure => "present",}
+  package { 'curl': ensure => installed }
+
+  group { "datawinners": ensure => "present", }
 
   user { "datawinners":
     ensure     => "present",
     managehome => true,
-    gid => "datawinners",
-    require => Group["datawinners"],
+    gid        => "datawinners",
+    require    => Group["datawinners"],
   }
 
-######## Postgres installation ############
+  # ####### Postgres installation ############
   class { "postgresql::server":
     config_hash => {
-      'listen_addresses' => '*',
-      'postgres_password' => 'postgres',
-      'ip_mask_allow_all_users' => '0.0.0.0/0',
+      'listen_addresses'           => '*',
+      'postgres_password'          => 'postgres',
+      'ip_mask_allow_all_users'    => '0.0.0.0/0',
       'ip_mask_deny_postgres_user' => '0.0.0.0/32',
-    },
+    }
+    ,
   }
 
- postgresql::database_user{ 'mangrove':
+  postgresql::database_user { 'mangrove':
     # TODO: ensure is not yet supported
-    #ensure        => present,
+    # ensure        => present,
     password_hash => postgresql_password('mangrove', 'mangrove'),
     require       => Class['postgresql::server'],
   }
 
   $pg_conf_include_file = "${postgresql::params::confdir}/postgresql_puppet_extras.conf"
+
   file { $pg_conf_include_file:
     content => 'standard_conforming_strings = off',
     notify  => Service['postgresql'],
   }
 
-######## Python installation ############
-  class {'python': virtualenv => true}
+  # ####### Python installation ############
+  class { 'python':
+    virtualenv => true
+  }
+
   python::virtualenv { '/home/datawinners/virtual_env/datawinner':
-    ensure       => present,
-    owner        => "datawinners",
-    group        => "datawinners",
-  }
-  python::pip {"pip":
-    virtualenv  => '/home/datawinners/virtual_env/datawinner',
-    owner       => 'datawinners',
-    require     => User["datawinners"],
+    ensure => present,
+    owner  => "datawinners",
+    group  => "datawinners",
   }
 
+  python::pip { "pip":
+    virtualenv => '/home/datawinners/virtual_env/datawinner',
+    owner      => 'datawinners',
+    require    => User["datawinners"],
+  }
 
-############### Apache Couchdb configuration ###########
-## couchdb user/group is created as part of the installation
+  # ############## Apache Couchdb configuration ###########
+  # # couchdb user/group is created as part of the installation
 
 
-#  class {"tomcat":
-#  }
+  #  class {"tomcat":
+  #  }
 
-#
-#node dev inherits base_system{
-#  class{"datawinnersapp":}
-#
-#}
-#
-#node ci inherits dev {
-#  class { 'jenkins': }
-#
-#  jenkins::plugin {
-#    'git' : ;
-#  }
+  #
+  # node dev inherits base_system{
+  #  class{"datawinnersapp":}
+  #
+  #}
+  #
+  # node ci inherits dev {
+  #  class { 'jenkins': }
+  #
+  #  jenkins::plugin {
+  #    'git' : ;
+  #  }
 }
 
 node default {
+  class { "couchdb": }
 
-   class { "couchdb":}
-   
-   couchdb::instance { "couchdbmain" :
-		require => Class['couchdb'], 
-		service_name => "couchdbmain",
-		database_dir => "/opt/apache-couchdb/var/lib/couchdbmain",
-	 }
+  couchdb::instance { "couchdbmain":
+    require      => Class['couchdb'],
+    service_name => "couchdbmain",
+    database_dir => "/opt/apache-couchdb/var/lib/couchdbmain",
+  }
 
-
-   couchdb::instance { "couchdbfeed" :
-		require => Class['couchdb'],
-		service_name => "couchdbfeed",
-                database_dir => "/opt/apache-couchdb/var/lib/couchdbfeed",
-		port => "7984", 
-   }  
+  couchdb::instance { "couchdbfeed":
+    require      => Class['couchdb'],
+    service_name => "couchdbfeed",
+    database_dir => "/opt/apache-couchdb/var/lib/couchdbfeed",
+    port         => "7984",
+  }
 }
-
 
--- modules/couchdb/manifests/init.pp
+++ modules/couchdb/manifests/init.pp
@@ -1,57 +1,72 @@
 class couchdb (
   $package_location = 'https://github.com/mangroveorg/dwpkg/raw/master/couchdb/apache-couchdb_1.3.1-1_amd64.deb',
-  $package_name = 'apache-couchdb_1.3.1-1_amd64.deb'
-)
- {
+  $package_name     = 'apache-couchdb_1.3.1-1_amd64.deb') {
+  group { "couchdb": ensure => "present", }
 
-  group {"couchdb": ensure => "present",}
+  user { "couchdb":
+    ensure     => present,
+    gid        => "couchdb",
+    managehome => false,
+    shell      => '/bin/sh',
+    home       => '/opt/apache-couchdb',
+  }
 
-  user { "couchdb" : 
-	ensure => present,
-	gid => "couchdb",
-  	managehome => false,
-	shell => '/bin/sh',
-	home => '/opt/apache-couchdb',
-  }
-  
-  $required_packages = ['wget','libicu-dev', 'libmozjs185-dev', 'libcurl4-gnutls-dev', 'libtool', 'erlang-base-hipe','erlang-eunit','erlang-nox','erlang-xmerl', 'erlang-inets']
-  package { $required_packages :
-	ensure => 'installed',
-  }
+  $required_packages = [
+    'wget',
+    'libicu-dev',
+    'libmozjs185-dev',
+    'libcurl4-gnutls-dev',
+    'libtool',
+    'erlang-base-hipe',
+    'erlang-eunit',
+    'erlang-nox',
+    'erlang-xmerl',
+    'erlang-inets']
+
+  package { $required_packages: ensure => 'installed', }
 
   exec { 'download':
     require => Package[$required_packages],
-    cwd => '/opt/',
-    path => [ '/usr/local/bin', '/usr/bin', '/bin', ],   
+    cwd     => '/opt/',
+    path    => ['/usr/local/bin', '/usr/bin', '/bin',],
     command => "wget -q ${package_location} -O ${package_name}",
     timeout => 120,
   }
-  
-  file { "/opt/${package_name}" :
+
+  file { "/opt/${package_name}":
     require => Exec['download'],
-    ensure => 'present',
-    mode => '0755',	
+    ensure  => 'present',
+    mode    => '0755',
   }
-  
+
   exec { 'install-couchdb':
-   require => File["/opt/${package_name}"],
-   path => [ '/usr/local/bin', '/usr/sbin', '/sbin', '/usr/bin', '/bin', ],
-   cwd => '/opt/',
-   command => "dpkg -i ${package_name}",
-   timeout => 120,		
+    require => File["/opt/${package_name}"],
+    path    => ['/usr/local/bin', '/usr/sbin', '/sbin', '/usr/bin', '/bin',],
+    cwd     => '/opt/',
+    command => "dpkg -i ${package_name}",
+    timeout => 120,
   }
 
-  file { ["/opt/apache-couchdb/var",
-          "/opt/apache-couchdb/var/lib",
-	  "/opt/apache-couchdb/var/log", 
-	  "/opt/apache-couchdb/var/log/couchdb", 
-   	  "/opt/apache-couchdb/var/run",
-	  "/opt/apache-couchdb/var/run/couchdb"] :
-      require => Exec['install-couchdb'],
-      ensure => 'directory',
-      owner => 'couchdb',
+  file { "/opt/apache-couchdb":
+    recurse => true,
+    owner   => 'couchdb',
+    group   => 'couchdb',
+    mode    => '0555',
   }
+
+  file { [
+    "/opt/apache-couchdb/var",
+    "/opt/apache-couchdb/var/lib",
+    "/opt/apache-couchdb/var/log",
+    "/opt/apache-couchdb/var/log/couchdb",
+    "/opt/apache-couchdb/var/run",
+    "/opt/apache-couchdb/var/run/couchdb"]:
+    require => Exec['install-couchdb'],
+    ensure  => 'directory',
+    owner   => 'couchdb',
+    group   => 'couchdb',
+    mode    => '0755',
+  }
+
 }
-
-
 
