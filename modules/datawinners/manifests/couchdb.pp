56764dc2251093a9398b2d53c197d9e8b202dcee
Dileep Bapat <dileepbapat@gmail.com>
2013-08-14 13:56:45 +0530

    Fixing cyclic dependency

--- modules/datawinners/manifests/couchdb.pp
+++ modules/datawinners/manifests/couchdb.pp
@@ -5,13 +5,13 @@ class datawinners::couchdb {
   }
 
   couchdb::instance { "couchdbmain":
-    require      => Class['couchdb'],
+    require      => Class['::couchdb'],
     service_name => "couchdbmain",
     database_dir => "/opt/apache-couchdb/var/lib/couchdbmain",
   }
 
   couchdb::instance { "couchdbfeed":
-    require      => Class['couchdb'],
+    require      => Class['::couchdb'],
     service_name => "couchdbfeed",
     database_dir => "/opt/apache-couchdb/var/lib/couchdbfeed",
     port         => "7984",
