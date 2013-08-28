c3392b56a9a96d20dc0798411b849b6ceaf36269
ani <ani@dwdev.ani>
2013-08-28 10:23:48 +0530

    anishek | formatting

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,28 +1,28 @@
 
-class base_system($user_name = 'mangrover') {  
+class base_system ($user_name = 'mangrover') {
   class { "datawinners":
     user  => $user_name,
     group => $user_name,
   }
 
   exec { "install_mangrove_egg":
-    cwd     => "/home/${user_name}/workspace/mangrove/",
-    command => "/bin/sh -c \". /home/${user_name}/virtual_env/datawinners/bin/activate && cd /home/${user_name}/workspace/mangrove && /home/${user_name}/virtual_env/datawinners/bin/python  /home/${user_name}/workspace/mangrove/setup.py develop\"",
+    cwd       => "/home/${user_name}/workspace/mangrove/",
+    command   => "/bin/sh -c \". /home/${user_name}/virtual_env/datawinners/bin/activate && cd /home/${user_name}/workspace/mangrove && /home/${user_name}/virtual_env/datawinners/bin/python  /home/${user_name}/workspace/mangrove/setup.py develop\"",
     logoutput => "on_failure",
-    require => Class["datawinners"],
+    require   => Class["datawinners"],
   }
 }
 
-node /(dwdev)\..*/  { # dev
+node /(dwdev)\..*/ { # dev
   $user_name = $::env_sudo_user
-  class {"base_system":
-    user_name => $user_name 
-  }
+
+  class { "base_system": user_name => $user_name }
+
   file { "/home/${user_name}/workspace/datawinners/datawinners/local_settings.py":
-    source => "/home/${user_name}/workspace/datawinners/datawinners/config/local_settings_example.py",
-    ensure => present,
-    owner  => $user_name,
-    group  => $user_name,
+    source  => "/home/${user_name}/workspace/datawinners/datawinners/config/local_settings_example.py",
+    ensure  => present,
+    owner   => $user_name,
+    group   => $user_name,
     require => Class["datawinners"],
   }
 
@@ -33,25 +33,26 @@ node /(dwdev)\..*/  { # dev
       && /home/${user_name}/workspace/datawinners/build.sh rsdb\"",
     user      => "${user_name}",
     logoutput => "on_failure",
-    timeout => 1000,
+    timeout   => 1000,
     require   => [
       Exec["initialize-datawinners-environment"],
       File["/home/${user_name}/workspace/datawinners/datawinners/local_settings.py"],
       Exec["install_mangrove_egg"]],
-      notify=> Service["uwsgi"],
+    notify    => Service["uwsgi"],
   }
 }
 
 node /(dwci)\..*/ {
-  class {"base_system": }
-  class{ "datawinners::jenkins":  }
+  class { "base_system": }
+
+  class { "datawinners::jenkins": }
 }
 
 node /(dwqa)\..*/ {
   $user_name = 'mangrover'
-  class {"base_system":
-    user_name => $user_name 
-  }
+
+  class { "base_system": user_name => $user_name }
+
   file { "/home/${user_name}/workspace/datawinners/datawinners/local_settings.py":
     source => "/home/${user_name}/workspace/datawinners/datawinners/config/local_settings_test.py",
     ensure => present,
