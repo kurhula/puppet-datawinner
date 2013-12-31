c163ffd7ba30b6e1b904c4182da8967cf004de58
ajay <ajay_886@hotmail.com>
2013-12-31 14:52:10 +0530

    Ajay | #2698 | Corrected typo

--- modules/datawinners/manifests/memcached.pp
+++ modules/datawinners/manifests/memcached.pp
@@ -4,7 +4,7 @@ class datawinners::memcached ($owner, $group) {
     path                => '/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin',
     url                 => "https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz",
     destination_dir     => "/var/tmp",
-    owner   => "${user}",
+    owner   => "${owner}",
     group   => "${group}",
     postextract_command => "/bin/sh -c \"cd /var/tmp/libevent-2.0.21-stable && ./configure && make && sudo make install\""
   }->
@@ -13,7 +13,7 @@ class datawinners::memcached ($owner, $group) {
     path                => '/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin',
     url                 => "http://www.memcached.org/files/memcached-1.4.17.tar.gz",
     destination_dir     => "/var/tmp",
-    owner   => "${user}",
+    owner   => "${owner}",
     group   => "${group}",
     postextract_command => "/bin/sh -c \"cd /var/tmp/memcached-1.4.17 && ./configure && make && sudo make install\""
   }->
@@ -22,7 +22,7 @@ class datawinners::memcached ($owner, $group) {
     path                => '/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin',
     url                 => "https://launchpad.net/libmemcached/1.0/1.0.17/+download/libmemcached-1.0.17.tar.gz",
     destination_dir     => "/var/tmp",
-    owner   => "${user}",
+    owner   => "${owner}",
     group   => "${group}",
     postextract_command => "/bin/sh -c \"cd /var/tmp/libmemcached-1.0.17 && ./configure && make && sudo make install\"",
     require             => Package["libcloog-ppl0"],
