b3309c89bf8a133bff23efdd5de3edafc1a15a48
Deepthi-Chand <deepthia@thoughtworks.com>
2013-12-31 11:23:10 +0530

    DC,Ashwin | #2784 | Enabling rabbitmq management plugin

--- modules/datawinners/manifests/rabbitmq.pp
+++ modules/datawinners/manifests/rabbitmq.pp
@@ -1,8 +1,8 @@
 class datawinners::rabbitmq ($owner, $group) {
-  
-  package { "rabbitmq-server":
-    ensure => "installed"
+  package { "rabbitmq-server": ensure => "installed" } ->
+  exec { "install_rabbitmq_management_plugins": command => '/usr/lib/rabbitmq/lib/rabbitmq_server-2.7.1/sbin/rabbitmq-plugins enable rabbitmq_management', 
   } ->
+  exec { "restart_rabbitmq_server": command => 'service rabbitmq_server restart' } ->
   service { 'rabbitmq-server':
     ensure => 'running',
     enable => true,
