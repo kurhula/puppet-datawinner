a337a53980f76624c019db37406c1bef643f40cf
Dileep Bapat <dileepbapat@gmail.com>
2014-06-04 14:33:56 +0530

    Ashwin,Dileep | Removing duplicate ulimit config

--- modules/datawinners/manifests/couchdb.pp
+++ modules/datawinners/manifests/couchdb.pp
@@ -8,20 +8,6 @@ class datawinners::couchdb($user) {
         soft       => 65000,
   }
   
-  limits::limits { "${user}_nofile":
-        ensure     => present,
-        user       => "${user}",
-        limit_type => 'nofile',
-        hard       => 65000,
-        soft       => 65000,
-  }
-  file { "/etc/pam.d/common-session":
-    content => template('datawinners/etc/pam.d/common-session.erb')
-  }
-  file { "/etc/pam.d/common-session-noninteractive":
-    content => template('datawinners/etc/pam.d/common-session.erb')
-  }
-  
   # ############## Apache Couchdb configuration ###########
   # # couchdb user/group is created as part of the installation
   class { "::couchdb":
